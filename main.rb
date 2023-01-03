ffb6b83b2b155a7e3da9c40c073e9fd8b3510c1e
Terence Holmes <piece4ulmind@gmail.com>
2023-01-02 21:33:07 -0500

    Commit

---
+++ main.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+class SinatraWorker
+
+    set :default_content_type, 'application/json'
+  
+    # Reads all the teams and uniforms from the database
+    # Returns a JSON response with an array of all the team and uniform data
+    get '/teams' do
+      teams = Team.all
+      teams.to_json(include: :uniforms)
+    end
+    
+    # finds the team in the database using its ID
+    # sends a JSON-formatted response of the team and uniform data
+    get '/teams/:id' do
+      team = Team.find(params[:id])
+      team.to_json(include: :uniforms)
+    end
+  
+    # adds the team and uniforms to the database
+    post "/teams" do
+      Team.create(params).to_json(include: :uniforms)
+    end
+  
+    # finds the team in the database using the ID
+    # deletes the team
+    delete "/teams/:id" do
+      team = Team.find(params[:id])
+      team.destroy
+    end      
+
+end
